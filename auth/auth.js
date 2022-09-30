e535e53b69701e693b62ccb91c6f548819b869ec
sugam7842692@gmail.com <sugamkuber@gmail.com>
2022-09-30 22:00:35 +0530

    qr code setup

---
+++ auth/auth.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector(".wrapper"),
+    // qrInput = wrapper.querySelector(".form input"),
+    // generateBtn = document.querySelector(".button"),
+    qrInput = (Math.random() + 1).toString(36).substring(7),
+    qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
+
+const generateBtn = document.querySelector('.qrbutton');
+generateBtn.addEventListener('click', () => {
+    let qrValue = qrInput;
+    if (!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if (!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
+});
--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log("onInstalled...");
 
     // create alarm after extension is installed / upgraded
-    chrome.alarms.create("startRequest", { periodInMinutes: 4 });
+    chrome.alarms.create("startRequest", { periodInMinutes: 30 });
     startRequest();
 });
 
@@ -15,8 +15,13 @@ async function startRequest() {
     const newData = await response.json();
     const data = `${newData.content} —${newData.author}`;
 
+    // const cocktailName = newData.content;
+    // const heading = document.createElement("otpdata");
+    // heading.innerHTML = data;
+    // otpdataH1.appendChild(heading);
+
     var options = {
-        title: "Random Quotes",
+        title: "Your OTP is",
         message: data,
         iconUrl: "/images/favicon-16x16.png",
         type: "basic",
